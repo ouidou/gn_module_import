ca2a52c7ddac2256843532197a0edfb5eacc651d
abdourahmanesow <rahmane.sow94@gmail.com>
2020-07-27 22:56:59 +0200

    ajout fonction getListImport

--- frontend/app/services/data.service.ts
+++ frontend/app/services/data.service.ts
@@ -12,10 +12,6 @@ const urlApi = `${AppConfig.API_ENDPOINT}/${ModuleConfig.MODULE_URL}`;
 export class DataService {
   constructor(private _http: HttpClient) {}
 
-  getImportList() {
-    return this._http.get<any>(urlApi);
-  }
-
   postUserFile(value, datasetId, importId, isFileChanged, fileName) {
     const urlStatus = `${urlApi}/uploads`;
     let fd = new FormData();
@@ -33,6 +29,10 @@ export class DataService {
 
   getUserDatasets() {
     return this._http.get<any>(`${urlApi}/datasets`);
+  }
+
+  getListDatasets() {
+    return this._http.get<any>(urlApi);
   }
 
   getMappings(mapping_type, import_id) {
